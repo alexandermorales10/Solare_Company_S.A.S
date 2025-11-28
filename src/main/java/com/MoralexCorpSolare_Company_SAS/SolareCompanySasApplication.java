fa6bf43a161ef0b8d78a9172d8b0d733e1497a2d
alexandermorales10 <alexandermoralesmorales10@gmail.com>
2025-11-28 17:55:55 -0500

    Primer commit

---
+++ src/main/java/com/MoralexCorpSolare_Company_SAS/SolareCompanySasApplication.java
@@ -0,0 +1,13 @@
+package com.MoralexCorpSolare_Company_SAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolareCompanySasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolareCompanySasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MoralexCorpSolare_Company_SAS/SolareCompanySasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoralexCorpSolare_Company_SAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolareCompanySasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
